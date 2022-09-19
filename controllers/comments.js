a096603a809529dfce52093104a0fe0bf2d57a67
Erika Rios <rios.erika.tatiana@gmail.com>
2022-09-19 16:10:28 -0700

    like and delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -9,8 +9,36 @@ module.exports = {
         post: req.params.id,
       });
       console.log("Comment has been added!");
-      res.redirect("/post/"+req.params.id);
+      res.redirect("/post/" + req.params.id);
     } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      const comment = await Comment.findById({ _id: req.params.id });
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Find post by id
+      let comment = await Comment.findById({ _id: req.params.id });
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      //res.redirect(`/post/${comment.post}`);
       console.log(err);
     }
   },
